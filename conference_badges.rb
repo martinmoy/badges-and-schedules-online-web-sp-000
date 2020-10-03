5324e56e0aad038ff068e8d197ef9031f9857fee
Martin Moy <martinmoy1991@gmail.com>
2020-10-03 12:17:52 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,17 +1,18 @@
+require "pry"
+
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-
 def batch_badge_creator(array)
-  array.collect{
-    |name| badge_maker(name)
+  array.collect{ |name|
+    badge_maker(name)
   }
 end
 
 def assign_rooms(array)
-  array.each_with_index.collect {
-    |array, index| "Hello, #{array}! You'll be assigned to room #{index+1}!"
+  array.each_with_index.collect{|name,index|
+  "Hello, #{name}! You'll be assigned to room #{index+1}!"
   }
 end
 
